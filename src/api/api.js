1328e2333a1d7e870f217b69201c3a5211682618
Juan de Santos <juanj.desantos@gmail.com>
2023-07-17 18:14:37 +0200

    load books from api

--- src/api/api.js
+++ src/api/api.js
@@ -2,28 +2,70 @@ import { Formik, Form, Field } from "formik";
 import { useState } from "react";
 
 const Api = () => {
-  //const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState([]);
+  const [wish, setWish] = useState([]);
   //console.log(books);
+
+  function handleChange(e) {
+    const title = e.target.title;
+
+    switch (title) {
+      case "wish":
+        setWish(e.target.checked);
+        break;
+      default:
+    }
+  }
+
   return (
-    <Formik
-      initialValues={{ search: "" }}
-      onSubmit={async (values) => {
-        try {
-          const response = await fetch(
-            `https://www.googleapis.com/books/v1/volumes?q=${values.search}`
-          );
-          const data = await response.json();
-          console.log(data);
-          //setBooks(data.results);
-        } catch (error) {
-          console.error(error);
-        }
-      }}
-    >
-      <Form>
-        <Field name="search" />
-      </Form>
-    </Formik>
+    <div>
+      <Formik
+        initialValues={{ search: "" }}
+        onSubmit={async (values) => {
+          try {
+            const response = await fetch(
+              `https://www.googleapis.com/books/v1/volumes?q=${values.search}`
+            );
+            const data = await response.json();
+            console.log(data);
+            if (Array.isArray(data.items)) {
+              setBooks(data.items);
+            } else {
+              console.log(
+                'La respuesta de la API no contiene un array en la propiedad "results".'
+              );
+            }
+          } catch (error) {
+            console.error(error);
+          }
+        }}
+      >
+        <Form>
+          <Field name="search" />
+        </Form>
+      </Formik>
+
+      <div>
+        <div>
+          {books.map((book) => (
+            <article key={book.id}>
+              {book.volumeInfo && book.volumeInfo.imageLinks && (
+                <img src={book.volumeInfo.imageLinks.thumbnail} alt="" />
+              )}
+              <div>
+                <div>to favourite</div>
+                <input
+                  type="checkbox"
+                  name="wish"
+                  onChange={handleChange}
+                  value={wish}
+                ></input>
+              </div>
+            </article>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
 
